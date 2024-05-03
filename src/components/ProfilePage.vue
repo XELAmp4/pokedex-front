e7b0d2324433b6450e782f8091c2bea1b21ce971
XELAmp4 <alexandre.bellet0321@gmail.com>
2024-05-03 16:42:02 +0200

    design rapide de la page de profil

--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -1,32 +1,33 @@
 <template>
-    <BurgerMenu/>
-    <h1>Profil</h1>
+    <div>
+        <BurgerMenu/>
 
-    <section class="profile-container">
-        <div class="profile-header">
-            <img v-if="userData" class="profile-image" :src="userData.imgURL" alt="profile picture">
-            <div class="profile-infos">
-                <span v-if="userData" class="profile-username">{{ userData.username }}</span>
-                <span v-if="userData" class="profile-role">{{ userRole }}</span>
+        <section class="profile-container">
+            <div class="profile-header">
+                <img v-if="userData" class="profile-image" :src="userData.imgURL" alt="profile picture">
+                <div class="profile-infos">
+                    <span v-if="userData" class="profile-username">{{ userData.username }}</span>
+                    <span v-if="userData" :class='"profile-role "+ userRole'>{{ userRole }}</span>
+                </div>
             </div>
-        </div>
-        <div class="profile-body">
-            <div v-if="userData.pkmnSeen" class="profile-datas">
-                <span class="profile-label">Pokemons seen</span>
-                <span v-if="userData.pkmnSeen" class="profile-dataPercentage">{{ getPercentage(userData.pkmnSeen.length) }}%</span>
-                <span v-if="userData.pkmnSeen" class="profile-dataNumber">{{ userData.pkmnSeen.length }}/{{ maxPokemon }}</span>
+            <div class="profile-body">
+                <div v-if="userData.pkmnSeen" class="profile-datas">
+                    <span class="profile-label">Pokemons seen</span>
+                    <span v-if="userData.pkmnSeen" class="profile-dataPercentage">{{ getPercentage(userData.pkmnSeen.length) }}%</span>
+                    <span v-if="userData.pkmnSeen" class="profile-dataNumber">{{ userData.pkmnSeen.length }}/{{ maxPokemon }}</span>
+                </div>
+                <div v-if="userData.pkmnCatch" class="profile-datas">
+                    <span class="profile-label">Pokemons catch</span>
+                    <span v-if="userData.pkmnCatch" class="profile-dataPercentage">{{ getPercentage(userData.pkmnCatch.length) }}%</span>
+                    <span v-if="userData.pkmnCatch" class="profile-dataNumber">{{ userData.pkmnCatch.length }}/{{ maxPokemon }}</span>
+                </div>
             </div>
-            <div v-if="userData.pkmnCatch" class="profile-datas">
-                <span class="profile-label">Pokemons catch</span>
-                <span v-if="userData.pkmnCatch" class="profile-dataPercentage">{{ getPercentage(userData.pkmnCatch.length) }}%</span>
-                <span v-if="userData.pkmnCatch" class="profile-dataNumber">{{ userData.pkmnCatch.length }}/{{ maxPokemon }}</span>
+            <div class="profile-cta">
+                <button @click="letsEdit" class="profile-btnEdit">Edit profile</button>
+                <button @click="disconnectUser" class="profile-btnLogout">Log out</button>
             </div>
-        </div>
-        <div class="profile-cta">
-            <button @click="letsEdit" class="profile-btnEdit">Edit profile</button>
-            <button @click="disconnectUser" class="profile-btnLogout">Log out</button>
-        </div>
-    </section>
+        </section>
+    </div>
 </template>
 
 
@@ -82,42 +83,100 @@ export default {
 
 <style lang="sass" scoped>
 @import '/src/styles/main.sass'
-.profile
-    &-container
-        font-family: $ff1
-        background: $light
-        width: 90vw
-        position: absolute
-        top: 50%
-        left: 50%
-        transform: translate(-50%, -50%)
-        padding: 2.5vw
-        border: 1px solid grey
-        border-radius: 30px 
 
-    &-header
-        display: flex
-        align-items: center
+.profile-container
+    font-family: $ff1
+    background: $light
+    width: 90vw
+    margin: auto
+    padding: 20px
+    border-radius: 20px
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1)
+    text-align: center
+    position: absolute
+    top: 50%
+    left: 50%
+    transform: translate(-50%,-50%)
 
-    &-infos
-        display: flex
-        flex-direction: column
+.profile-header
+    display: flex
+    align-items: center
+    justify-content: center
+    margin-bottom: 20px
 
-    &-image
-        border-radius: 100%
-        width: 100px
+.profile-image
+    border-radius: 50%
+    width: 100px
+    height: 100px
+    object-fit: cover
+    margin-right: 20px
 
-    &-body
-        width: 100%
-        display: flex
-        justify-content: space-between
+.profile-infos
+    display: flex
+    flex-direction: column
 
-    &-datas
-        width: 48%
-        border: 1px purple solid
-        display: flex
-        justify-content: center
-        align-items: center
-        flex-direction: column
-        
+.profile-username
+    font-size: 24px
+    font-weight: bold
+    margin-bottom: 5px
+
+.profile-role
+    color: grey
+    background-color: lighten(grey, 40%)
+    border: 1px solid grey
+    border-radius: 5px
+    padding: 2px 5px
+
+    &.admin
+        color: #ffcc00
+        background-color: lighten(#ffcc00, 45%)
+        border: 1px solid #ffcc00
+
+    &.trainer
+        color: #007bff
+        background-color: lighten(#007bff, 45%)
+        border: 1px solid #007bff
+
+.profile-body
+    display: flex
+    justify-content: space-around
+    margin-bottom: 20px
+
+.profile-datas
+    display: flex
+    flex-direction: column
+    align-items: center
+
+.profile-label
+    font-weight: bold
+    margin-bottom: 5px
+
+.profile-dataPercentage
+    font-size: 20px
+    color: #42b883
+    margin-bottom: 5px
+
+.profile-dataNumber
+    color: #666
+
+.profile-cta
+    display: flex
+    justify-content: center
+
+.profile-btnEdit,
+.profile-btnLogout
+    padding: 10px 20px
+    margin: 0 10px
+    border: none
+    border-radius: 5px
+    font-size: 16px
+    cursor: pointer
+
+.profile-btnEdit
+    background-color: #42b883
+    color: #fff
+
+.profile-btnLogout
+    background-color: #f04e4e
+    color: #fff
 </style>
